7d48008f339ecf3937469011562bd921a74171a7
Aman Agarwal <aman_k_agarwal@hotmail.com>
2018-04-14 17:46:56 -0700

    Testing subreddit get requests

--- test.py
+++ test.py
@@ -2,7 +2,10 @@ import requests
 from pprint import pprint
 import json
 
-r = requests.get(r'https://www.reddit.com/user/HammerJayce/about/.json', headers = {'User-agent': 'your bot 0.1'})
+r = requests.get(r'https://www.reddit.com/r/udub/top.json', headers = {'User-agent': 'your bot 0.1'})
+# I can get number of upvotes
+# I can get the link of the post with the comments "permalink"
+# I can get the title of the post which is a good indicator of what
+# type of post it is.
 python_dict = json.loads(r.text)
-
 pprint(python_dict)
