7b059b063cf128c623220f527eaf4f23856c9214
Aman Agarwal <aman_k_agarwal@hotmail.com>
2018-08-13 18:29:03 +0200

    back2work

--- data_extraction.py
+++ data_extraction.py
@@ -15,7 +15,7 @@ def get_dict_from_json(url):
     python_dict = json.loads(send_request.text)
     return (python_dict)
 
-def get_all_data(start_date, end_date): #Can make this function such so that I can get data between two particular days
+def get_all_data(start_date, end_date): # Can make this function such so that I can get data between two particular days
     count = 0
     after_tag = ""
     main_dict = {}
@@ -30,6 +30,7 @@ def get_all_data(start_date, end_date): #Can make this function such so that I c
             post_data = post_dictionary["data"] # This is also a dictionary
             post_timestamp = post_data["created_utc"]
             if (post_timestamp >= start_date and post_timestamp <= end_date): #need a more efficient way to check this
+                                                                              #deal with this
                 post_author = post_data["author"]
                 post_title = post_data["title"]
                 post_description = post_data["selftext"]
@@ -45,20 +46,19 @@ def parse_comments(comments_link):
     comments_url = "http://www.reddit.com" + comments_link + ".json"
     python_dict = get_dict_from_json(comments_url)
 
-'''
-url = "https://www.reddit.com/r/udub/comments/83hxt3/question_regarding_direct_to_coe_admission/.json"
+# Working on the comments section extraction
 array_of_dicts = get_dict_from_json(url)
-pprint (array_of_dicts)
-total_comments = len(array_of_dicts) #this includes title description
+pprint(array_of_dicts)
+total_comments = len(array_of_dicts)
 for i in range(len(array_of_dicts)):
-    if (i != 0): # this means there exists a comment other than the description
+    if  (i != 0): # Why? This means there exists a comment other that the description
         comments_outer_dict = array_of_dicts[i]
         comments_array = comments_outer_dict["data"]["children"]
-        for j in range(len(comments_array)):
+        for j in range(len(comment_array):
             comment_data_outer_dict = comments_array[j]
-            comment_data_inner_dict = comment_data_outer_dict["data"]
+            comments_data_inner_dict = comment_data_outer_dict["data"]
             comment_timestamp = comment_data_inner_dict["created_utc"]
             comment_author = comment_data_inner_dict["author"]
             comment_body = comment_data_inner_dict["body"]
-            #Complete this, there are replies to comments also, so need a good way to store it. (maybe a key called "replies for the comment timestamp key?")
-'''
+            comment_title = comment_data_inner_dict["title"]
+            comment_next_after_tag = comment_next_after_tag["after"]
